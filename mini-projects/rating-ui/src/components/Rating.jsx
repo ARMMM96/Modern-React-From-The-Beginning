9365811f5dce2cfef6e8a238224958fc39584da5
Ahmed Rabie <ahmedrabie@192.168.1.73>
2025-08-05 19:51:49 +0300

    Lists

--- mini-projects/rating-ui/src/components/Rating.jsx
+++ mini-projects/rating-ui/src/components/Rating.jsx
@@ -1,5 +1,15 @@
 const Rating = () => {
-  return <div className="rating-container">Rate your Eperince</div>;
+  const stars = Array.from({ length: 5 }, (_, i) => i + 1);
+  return (
+    <div className="rating-container">
+      <h2>Rate your Eperince</h2>
+      <div className="stars">
+        {stars.map((star) => (
+          <span className="star">{"\u2605"}</span>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Rating;
