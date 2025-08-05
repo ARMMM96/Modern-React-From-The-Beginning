86747567172806250b952f15a8248810b9618574
Ahmed Rabie <ahmedrabie@192.168.1.73>
2025-08-05 20:32:05 +0300

    Handling Events

--- mini-projects/rating-ui/src/components/Rating.jsx
+++ mini-projects/rating-ui/src/components/Rating.jsx
@@ -1,11 +1,16 @@
 const Rating = () => {
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
+
+  const clicked = (index) => console.log("cliked", index);
+
   return (
     <div className="rating-container">
       <h2>Rate your Eperince</h2>
       <div className="stars">
-        {stars.map((star) => (
-          <span className="star">{"\u2605"}</span>
+        {stars.map((star, index) => (
+          <span onClick={() => clicked(index)} className="star">
+            {"\u2605"}
+          </span>
         ))}
       </div>
     </div>
