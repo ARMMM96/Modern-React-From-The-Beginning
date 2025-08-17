919354fea419c3cf2553d7d87ea87300756fe528
Ahmed Rabie <ahmedrabie@MacBook-Pro-Ahmed.local>
2025-08-17 22:49:45 +0300

    useRef In Action Uncontrolled Inputs

--- mini-projects/simple-timer/src/App.jsx
+++ mini-projects/simple-timer/src/App.jsx
@@ -1,15 +1,34 @@
-import { useState } from 'react'
-
+import { useRef } from "react";
 
 function App() {
- 
+  const inputRef = useRef(null);
+  const submit = () => {
+    console.log(inputRef.current);
+    console.log(inputRef.current.value);
+    inputRef.current.focus();
+    inputRef.current.style.backgroundColor = "red";
+    inputRef.current.style.color = "white";
+    inputRef.current.setAttribute("placeholder", "updated");
+  };
+  console.log(inputRef);
   return (
-    <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="max-w-md mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg text-center">
+      <h2 className="text-2xl font-bold mb-4">useRef Example</h2>
+
+      <input
+        type="text"
+        className="w-full p-2 border rounded-lg"
+        placeholder="Type something..."
+        ref={inputRef}
+      />
+      <button
+        onClick={submit}
+        className="mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+      >
+        Submit
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
