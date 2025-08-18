9829ec597af86c8e898c65c9c9ee99cd98ecf662
Ahmed Rabie <ahmedrabie@MacBook-Pro-Ahmed.local>
2025-08-18 09:02:33 +0300

    Persist Timer Across ReRenders

--- mini-projects/simple-timer/src/App.jsx
+++ mini-projects/simple-timer/src/App.jsx
@@ -1,34 +1,52 @@
-import { useRef } from "react";
+import { useState, useRef } from "react";
 
-function App() {
-  const inputRef = useRef(null);
-  const submit = () => {
-    console.log(inputRef.current);
-    console.log(inputRef.current.value);
-    inputRef.current.focus();
-    inputRef.current.style.backgroundColor = "red";
-    inputRef.current.style.color = "white";
-    inputRef.current.setAttribute("placeholder", "updated");
+const App = () => {
+  const timerRef = useRef(null);
+
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const toggleTimer = () => {
+    if (isRunning) {
+      // Clear interval to stop the timer
+      clearInterval(timerRef.current);
+      timerRef.current = null;
+    } else {
+      // Start timer
+      timerRef.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    }
+    setIsRunning(!isRunning);
   };
-  console.log(inputRef);
+
+  const resetTimer = () => {
+    clearInterval(timerRef.current);
+    setIsRunning(false);
+    setTime(0);
+    timerRef.current = null;
+  };
+
+
   return (
     <div className="max-w-md mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg text-center">
-      <h2 className="text-2xl font-bold mb-4">useRef Example</h2>
-
-      <input
-        type="text"
-        className="w-full p-2 border rounded-lg"
-        placeholder="Type something..."
-        ref={inputRef}
-      />
-      <button
-        onClick={submit}
-        className="mt-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-      >
-        Submit
-      </button>
+      <h2 className="text-4xl font-semibold mt-4">
+        Timer: {time}
+      </h2>
+        <button
+          onClick={toggleTimer}
+          className="mt-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 mr-3"
+        >
+          {isRunning ? "Pause" : "Start"}
+        </button>
+        <button
+          onClick={resetTimer}
+          className="mt-3 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600"
+        >
+          reset
+        </button>
     </div>
   );
-}
+};
 
 export default App;
